e79903139139eb9ebcab91929171eba0f467c21d
SHUVO <im.computer.engineeer@gmail.com>
2025-05-19 15:30:55 +0600

    Set Timeout and Set interval learning

---
+++ dynamic-practice/interval.js
@@ -0,0 +1,24 @@
+
+// setTimeout 
+
+// console.log (1);
+// console.log (2);
+// console.log (3);
+// console.log (4);
+// console.log (5);
+// console.log (6);
+
+// setTimeout (() =>{
+//     console.log (7);
+// }, 4000) ;
+
+
+
+
+// setInterval (()=>{});
+
+let count = 0;
+setInterval (() => {
+    console.log (count);
+    count++;
+}, 1000);
